3fb4e78173484d538e7c604b8eb6a40111d81059
CyberAxe3108 <crismaramaro31@gmail.com>
2026-07-16 18:12:31 -0400

    modified

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -1,6 +1,6 @@
 
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function getAllPlaylists() {
     const response = await fetch(`${BASE_URL}/playlists`)
